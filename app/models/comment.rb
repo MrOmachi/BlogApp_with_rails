146d6bfa84caff61dda066a4bb2154a279cf734a
MrOmachi <apple@APPLEs-MacBook-Air.local>
2022-11-23 23:00:24 -0800

    Create comments  model and methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
-# class Comment < ApplicationRecord
-#      belongs_to :author, class_name: 'User'
-#      belongs_to :post
-#      after_save :update_comment_counter
+class Comment < ApplicationRecord
+     belongs_to :author, class_name: 'User'
+     belongs_to :post
+     after_save :update_comment_counter
 
-#      def update_comment_counter
-#           post.update(comments_counter: post.comments.all.length)
-#      end
+     def update_comment_counter
+          post.update(comments_counter: post.comments.all.length)
+     end
 
-# end
+end
