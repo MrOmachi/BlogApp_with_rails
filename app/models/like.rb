898ace165ba51161c6d1bca03736bf4c27128131
MrOmachi <apple@APPLEs-MacBook-Air.local>
2022-11-23 23:04:25 -0800

    Fix linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
-     belongs_to :author, class_name: 'User'
-     belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-     after_save :update_like_counter
+  after_save :update_like_counter
 
-     def update_like_counter
-          post.update(like_counter: post.likes.all.length)
-     end
+  def update_like_counter
+    post.update(like_counter: post.likes.all.length)
+  end
 end
