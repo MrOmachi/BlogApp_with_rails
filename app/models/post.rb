76e82a1bdca1d472767a4c63fe3def6d60030d72
MrOmachi <apple@APPLEs-MacBook-Air.local>
2022-11-23 22:59:07 -0800

    Create post model and methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
-# class Post < ApplicationRecord
-#      belongs_to :author, class_name: 'User'
-#      has_many :likes, foreign_key: :post_id
-#      has_many :comments, foreign_key: :post_id
+class Post < ApplicationRecord
+     belongs_to :author, class_name: 'User'
+     has_many :likes, foreign_key: :post_id
+     has_many :comments, foreign_key: :post_id
 
-#      after_save :update_post_counter
+     after_save :update_post_counter
 
-#      def five_recent_comments
-#           comments.order(created_at: :desc).limit(5)
-#      end
+     def five_recent_comments
+          comments.order(created_at: :desc).limit(5)
+     end
 
-#      def update_post_counter
-#           author.update(posts_counter: author.posts.all.length)
-#      end
+     def update_post_counter
+          author.update(posts_counter: author.posts.all.length)
+     end
 
-# end
+end
