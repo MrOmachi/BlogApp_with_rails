06a84be9ab3f1c58890adb13272141618bf5b89d
MrOmachi <apple@APPLEs-MacBook-Air.local>
2022-11-24 16:45:52 -0800

    Validates post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,19 @@
-# class Post < ApplicationRecord
-#   belongs_to :author, class_name: 'User'
-#   has_many :likes, foreign_key: :post_id
-#   has_many :comments, foreign_key: :post_id
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
 
-#   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-#   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-#   validates likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
-#   after_save :update_post_counter
+  after_save :update_post_counter
 
-#   def five_recent_comments
-#     comments.order(created_at: :desc).limit(5)
-#   end
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
-#   def update_post_counter
-#     author.update(posts_counter: author.posts.all.length)
-#   end
-# end
+  def update_post_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
+end
