5cc2e8c3ba5e4bb54e85a199807698973391a63c
Omachi John <omachi.john@yahoo.com>
2022-11-29 17:55:12 -0800

    Render show template for Post contoller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,11 +16,15 @@ RSpec.describe 'Post', type: :request do
           
           describe 'Get all related routes to post show template' do 
                before :each do 
-                    get '/users/:user_id/posts'
+                    get '/users/:user_id/posts/id'
                end
 
                it 'Returns success status for show routes of the post controller' do 
                     expect(response).to have_http_status(:success)
+               end
+
+               it 'Renders the show page for the post controller' do 
+                    expect(response).to render_template(:show)
                end
           end
      end
