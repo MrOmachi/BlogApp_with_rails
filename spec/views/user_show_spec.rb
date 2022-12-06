2a6f152d97696edcde2d0668f49f233f6cd401ea
Omachi John <omachi.john@yahoo.com>
2022-12-06 15:02:04 -0800

    Test show user name

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -59,38 +59,38 @@ RSpec.describe 'user / show ', type: :feature do
     expect(page).to have_content(@user.name)
   end
 
-  # it 'shows the user photo' do
-  #   expect(page.body).to include('https://picsum.photos/200')
-  # end
+  it 'shows the user photo' do
+    expect(page.body).to include('https://picsum.photos/200')
+  end
 
-  # it 'shows number of comments a post has' do
-  #   expect(page.body).to include('Comments: 3')
-  # end
+  it 'shows number of comments a post has' do
+    expect(page.body).to include('Comments: 3')
+  end
 
-  # it 'shows number of likes a post has' do
-  #   expect(page.body).to include('Likes: 0')
-  # end
+  it 'shows number of likes a post has' do
+    expect(page.body).to include('Likes: 0')
+  end
 
-  # it 'shows the title of the post' do
-  #   expect(page).to have_content(@last_post.title)
-  # end
+  it 'shows the title of the post' do
+    expect(page).to have_content(@last_post.title)
+  end
 
-  # it 'shows part of a post body (This is my first post)' do
-  #   expect(page).to have_content('This is my first post')
-  # end
+  it 'shows part of a post body (This is my first post)' do
+    expect(page).to have_content('This is my first post')
+  end
 
-  # it 'shows the 1st comment on a post (This is the 1st comment on the 4th post)' do
-  #   expect(page).to have_content('This is the 1st comment on the 4th post')
-  # end
+  it 'shows the 1st comment on a post (This is the 1st comment on the 4th post)' do
+    expect(page).to have_content('This is the 1st comment on the 4th post')
+  end
 
-  # it 'Not to show the Pagination button' do
-  #   expect(page.body).not_to include('Pagination')
-  # end
+  it 'Not to show the Pagination button' do
+    expect(page.body).not_to include('Pagination')
+  end
 
-  # it 'Redirect to post show page when a post is clicked' do
-  #   click_link(@last_post.title)
-  #   expect(current_path).to eq(user_post_path(@user, @last_post))
-  # end
+  it 'Redirect to post show page when a post is clicked' do
+    click_link(@last_post.title)
+    expect(current_path).to eq(user_post_path(@user, @last_post))
+  end
 end
 end
 
