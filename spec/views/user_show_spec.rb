4937828c007da6e209c62cccbd6108c55b6456f5
Omachi John <omachi.john@yahoo.com>
2022-12-06 14:31:29 -0800

    test Show posts likes

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -1,21 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'user show ', type: :feature do
-     before(:each) do
-       @user = User.create(name: 'John', photo: 'https://i.imgur.com/1JZrO7A.jpg', bio: 'bio1', posts_counter: 2)
-       @user.save!
-       @post = Post.create(title: 'post1', text: 'text1', comments_counter: 0, likes_counter: 0, author: @user)
-       @post2 = Post.create(title: 'post2', text: 'text2', comments_counter: 0, likes_counter: 0, author: @user)
-       @post3 = Post.create(title: 'post3', text: 'text3', comments_counter: 0, likes_counter: 0, author: @user)
-       visit user_path(id: @user)
-     end
-   
-     it 'should show the user photo' do
-       expect(page).to have_css("img[src*='https://i.imgur.com/1JZrO7A.jpg']")
-     end
+  before(:each) do
+    @user = User.create(name: 'Davon',
+                        photo: 'https://picsum.photos/200',
+                        bio: 'Nigerian Chemist',
+                        posts_counter: 0)
+    @any_post = Post.create(author_id: @user.id, title: 'Hello', text: 'This is any post', comments_counter: 0, likes_counter: 0)
+    Comment.create(post: @any_post, author_id: @user.id, text: 'This is the 1st comment on the 4th post')
+    Comment.create(post: @any_post, author_id: @user.id, text: 'This is the 2nd comment on the 4th post')
+    Comment.create(post: @any_post, author_id: @user.id, text: 'This is the 3rd comment on the 4th post')
+    visit user_post_path(@user, @any_post)
+  end
 
+  it 'shows number of likes a post has' do
+    expect(page.body).to include('Likes: 0')
+  end
+
+  # it 'shows the title of the post' do
+  #   expect(page).to have_content(@any_post.title)
+  # end
+
+  # it 'shows part of a post body (This is any post)' do
+  #   expect(page).to have_content('This is any post')
+  # end
+
+  it 'shows the user name of a comment' do
+    expect(page).to have_content('Davon')
+  end
+
+  it 'shows the comment a user left' do
+    expect(page).to have_content('This is the 2nd comment on the 4th post')
+  end
+   
      it 'should show the user name' do
       expect(page).to have_content(@user.name)
+    end
+
+    it 'shows number of comments a post has' do
+      expect(page.body).to include('Comments: 3')
     end
 end
 
